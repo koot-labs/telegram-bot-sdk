2ce1eca27a63c79654965002b5ee159d19f2a8fd
Alies Lapatsin <web.lapateen@gmail.com>
2022-07-10 16:55:59 +0300

    Add BotCommandScope objects (backport from SDK v4)

---
+++ src/Objects/AbstractCreateObject.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Telegram\Bot\Objects;
+
+use Illuminate\Support\Str;
+
+/**
+ * Class AbstractCreateObject
+ *
+ * This base class is used for when the user needs to create
+ * an object to be sent TO telegram.
+ */
+abstract class AbstractCreateObject extends BaseObject
+{
+    /**
+     * Create a new object.
+     *
+     * @param array $fields
+     */
+    public function __construct(array $fields = [])
+    {
+        parent::__construct($fields);
+    }
+
+    /**
+     * Magic method to set properties dynamically.
+     *
+     * @param $name
+     * @param $arguments
+     *
+     * @return $this
+     */
+    public function __call($name, $arguments)
+    {
+        $property = Str::snake($name);
+        $this->fields[$property] = $arguments[0];
+
+        return $this;
+    }
+}
